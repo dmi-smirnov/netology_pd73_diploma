659095b8fe0ae5ef33bfe59241501fc0cec8ae47
Dmitriy Smirnov <git@d-sm.ru>
2023-12-09 19:25:08 +0300

    created view for email verification

--- orders/api/urls.py
+++ orders/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import UserView
+from api.views import UserView, EmailVerification
 
 
 router = DefaultRouter()
 
 urlpatterns = [
     path('signup', UserView.as_view()),
+    path('verify_email', EmailVerification.as_view()),
     path('', include(router.urls)),
 ]
--- orders/api/views.py
+++ orders/api/views.py
@@ -1,6 +1,12 @@
+from django import forms
 from django.core.mail import EmailMessage
+from django.forms import Form
 from django.utils import timezone as django_timezone
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from api.serializers import UserSerializer
 from api.models import EmailVerificationCode, User
@@ -33,3 +39,59 @@ class UserView(CreateAPIView):
             created_user_evc.save()
 
         return result
+
+
+class EmailVerificationForm(Form):
+    email = forms.EmailField()
+    verification_code = forms.CharField(
+        min_length=EmailVerificationCode.LENGTH,
+        max_length=EmailVerificationCode.LENGTH
+    )
+
+
+class EmailVerification(APIView):
+    def post(self, request):
+        # Validating request data fields
+        errors = EmailVerificationForm(request.data).errors
+        if errors:
+            raise ValidationError(errors)
+        
+        req_email = request.data['email']
+
+        # Getting user from DB
+        try:
+            user = User.objects.get(email=req_email)
+        except User.DoesNotExist:
+            resp_data = {
+                'email': ['User with this email was not found.']
+            }
+            return Response(resp_data, status.HTTP_404_NOT_FOUND)
+        
+        # Checking the need for verification
+        if user.email_confirmed:
+            resp_data = {
+                'email': ['This email is already verified.']
+            }
+            return Response(resp_data, status.HTTP_400_BAD_REQUEST)
+
+        # Getting email verification code from DB
+        try:
+            user_evc = EmailVerificationCode.objects.get(user=user)
+        except EmailVerificationCode.DoesNotExist:
+            resp_data = {
+                'email': ['Verification code for this email was not found.']
+            }
+            return Response(resp_data, status.HTTP_404_NOT_FOUND)
+        
+        # Verification
+        req_evc = str(request.data['verification_code'])
+        if req_evc != user_evc.value:
+            resp_data = {
+                'verification_code': ['This code is invalid.']
+            }
+            return Response(resp_data, status.HTTP_400_BAD_REQUEST)
+        user.email_confirmed = True
+        user.is_active = True
+        user.save()
+        user_evc.delete()
+        return Response()
